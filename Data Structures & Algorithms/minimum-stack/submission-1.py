4abeb6f66d377cc4add98abf679bde4edfd8d093
Binh-Khang Nguyen <saltysalmonboi@gmail.com>
2026-08-13 20:27:11 +0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,27 @@
+class MinStack:
+    s: List
+    min_s: List
+
+    def __init__(self):
+        self.s = []
+        self.min_s = []
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+        if not self.min_s or val <= self.min_s[-1]:
+            self.min_s.append(val)
+
+    def pop(self) -> None:
+        if not self.s:
+            return None
+        val = self.s.pop()
+        if val == self.min_s[-1]:
+            self.min_s.pop()
+        
+    def top(self) -> int:
+        if not self.s:
+            return None
+        return self.s[-1]
+
+    def getMin(self) -> int:
+        return self.min_s[-1]
