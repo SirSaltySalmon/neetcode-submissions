4fc9cb88702e4aae68bff16806a695334b34615f
Binh-Khang Nguyen <saltysalmonboi@gmail.com>
2026-08-16 13:53:56 +0700

    Add: median-of-two-sorted-arrays - submission-2

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        # Binary search the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        total = len(nums1) + len(nums2)
+        half = (total + 1) // 2
+
+        l, r = 0, len(nums1)
+
+        while l <= r:
+            # i = number of elements taken from nums1
+            i = (l + r) // 2
+
+            # j = number of elements taken from nums2
+            j = half - i
+
+            # Values immediately to the left/right of each partition
+            nums1_left = nums1[i - 1] if i > 0 else float("-inf")
+            nums1_right = nums1[i] if i < len(nums1) else float("inf")
+
+            nums2_left = nums2[j - 1] if j > 0 else float("-inf")
+            nums2_right = nums2[j] if j < len(nums2) else float("inf")
+
+            # Valid partition
+            if nums1_left <= nums2_right and nums2_left <= nums1_right:
+                if total % 2 == 1:
+                    return float(max(nums1_left, nums2_left))
+                return (max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2
+
+            # Too many elements chosen from nums1
+            elif nums1_left > nums2_right:
+                r = i - 1
+
+            # Too many elements chosen from nums2
+            else:
+                l = i + 1
