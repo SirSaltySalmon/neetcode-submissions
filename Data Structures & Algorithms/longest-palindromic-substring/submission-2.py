01123eadbfea8ca5b68a99d8135e3f61585bef4b
Binh-Khang Nguyen <saltysalmonboi@gmail.com>
2026-09-26 01:44:06 +0700

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.py
@@ -0,0 +1,50 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # palindromes can be checked when
+        # you start from a center and expand outwards
+        # so for n^2...
+        # you can pick each center once
+        # and expand it out. each expansion is O(n)
+        # does n times so O(n^2)
+        # let's do recusion!
+
+        memo = {}
+
+        def expand(i):
+            if i in memo:
+                return memo[i]
+
+            l = i
+            r = i
+            cur_size = 1
+            # palindrome center either be odd or even len
+            # expand left and right by 1s to see the
+            # length of the center first.
+            same_results_centers = [i]
+            while l > 0 and s[l - 1] == s[i]:
+                l -= 1
+                cur_size += 1
+                same_results_centers.append(l)
+            while r < len(s) - 1 and s[r + 1] == s[i]:
+                r += 1
+                cur_size += 1
+                same_results_centers.append(r)
+            while (
+                l > 0 and r < len(s) - 1 and
+                s[l - 1] == s[r + 1]
+            ):
+                l -= 1
+                r += 1
+                cur_size += 2
+            
+            for cent in same_results_centers:
+                memo[cent] = (cur_size, l, r)
+
+            return (cur_size, l, r)
+
+        max_len = (0, 0, 0)
+        for i in range(len(s)):
+            max_len = max(expand(i), max_len)
+        
+        return s[max_len[1]:max_len[2]+1]
+        
